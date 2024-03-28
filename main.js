e47e2e13191cbf8ae54103a22ba8ad4cbbf4fae6
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2024-03-28 09:10:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    document.body.style.zoom = "90%";
+};
+
+function emBreve(value){
+    alert("Comecei recentemente a estudar esta ferramenta, em breve, será uma opção a mais para desenvolver novos projetos.")
+} ;
+
+
